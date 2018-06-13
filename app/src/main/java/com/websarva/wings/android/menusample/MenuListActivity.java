d2140330d6d96e309e63919f46d128d10bb7104e
disk3457 <dsakkyan0304@yahoo.co.jp>
2018-06-13 09:34:58 +0900

    0613

--- app/src/main/java/com/websarva/wings/android/menusample/MenuListActivity.java
+++ app/src/main/java/com/websarva/wings/android/menusample/MenuListActivity.java
@@ -20,24 +20,13 @@ import java.util.Map;
 
 public class MenuListActivity extends AppCompatActivity {
 
-	/**
-	 * リストビューを表すフィールド。
-	 */
+
 	private ListView _lvMenu;
 
-	/**
-	 * リストビューに表示するリストデータ。
-	 */
 	private List<Map<String, Object>> _menuList;
 
-	/**
-	 * SimpleAdapterの第4引数fromに使用する定数フィールド。
-	 */
 	private static final String[] FROM = {"name", "price"};
 
-	/**
-	 * SimpleAdapterの第5引数toに使用する定数フィールド。
-	 */
 	private static final int[] TO = {R.id.tvMenuName, R.id.tvMenuPrice};
 
 	@Override
@@ -45,124 +34,82 @@ public class MenuListActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_menu_list);
 
-		//画面部品ListViewを取得し、フィールドに格納。
 		_lvMenu = findViewById(R.id.lvMenu);
 
-		//SimpleAdapterで使用する定食メニューListオブジェクトをprivateメソッドを利用して用意し、フィールドに格納。
 		_menuList = createTeishokuList();
-		//SimpleAdapterを生成。
 		SimpleAdapter adapter = new SimpleAdapter(MenuListActivity.this, _menuList, R.layout.row, FROM, TO);
-		//アダプタの登録。
 		_lvMenu.setAdapter(adapter);
 
-		//リストタップのリスナクラス登録。
 		_lvMenu.setOnItemClickListener(new ListItemClickListener());
 
-		//コンテキストメニューをリストビューに登録。
 		registerForContextMenu(_lvMenu);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		//メニューインフレーターの取得。
 		MenuInflater inflater = getMenuInflater();
-		//オプションメニュー用xmlファイルをインフレイト。
 		inflater.inflate(R.menu.menu_options_menu_list, menu);
-		//親クラスの同名メソッドを呼び出し、その戻り値を返却。
 		return super.onCreateOptionsMenu(menu);
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		//選択されたメニューのIDを取得。
 		int itemId = item.getItemId();
-		//IDのR値による処理の分岐。
 		switch(itemId) {
-			//定食メニューが選択された場合の処理。
 			case R.id.menuListOptionTeishoku:
-				//定食メニューリストデータの生成。
 				_menuList = createTeishokuList();
 				break;
-			//カレーメニューが選択された場合の処理。
 			case R.id.menuListOptionCurry:
-				//カレーメニューリストデータの生成。
 				_menuList = createCurryList();
 				break;
 		}
-		//SimpleAdapterを選択されたメニューデータで生成。
 		SimpleAdapter adapter = new SimpleAdapter(MenuListActivity.this, _menuList, R.layout.row, FROM, TO);
-		//アダプタの登録。
 		_lvMenu.setAdapter(adapter);
-		//親クラスの同名メソッドを呼び出し、その戻り値を返却。
 		return super.onOptionsItemSelected(item);
 	}
 
 	@Override
 	public void onCreateContextMenu(ContextMenu menu, View view, ContextMenu.ContextMenuInfo menuInfo) {
-		//親クラスの同名メソッドの呼び出し。
 		super.onCreateContextMenu(menu, view, menuInfo);
-		//メニューインフレーターの取得。
 		MenuInflater inflater = getMenuInflater();
-		//コンテキストメニュー用xmlファイルをインフレイト。
 		inflater.inflate(R.menu.menu_context_menu_list, menu);
-		//コンテキストメニューのヘッダタイトルを設定。
 		menu.setHeaderTitle(R.string.menu_list_context_header);
 	}
 
 	@Override
 	public boolean onContextItemSelected(MenuItem item) {
-		//長押しされたビューに関する情報が格納されたオブジェクトを取得。
 		AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
-		//長押しされたリストのポジションを取得。
 		int listPosition = info.position;
-		//ポジションから長押しされたメニュー情報Mapオブジェクトを取得。
 		Map<String, Object> menu = _menuList.get(listPosition);
 
-		//選択されたメニューのIDを取得。
 		int itemId = item.getItemId();
-		//IDのR値による処理の分岐。
 		switch(itemId) {
-			//［説明を表示］メニューが選択された時の処理。
 			case R.id.menuListContextDesc:
-				//メニューの説明文字列を取得。
 				String desc = (String) menu.get("desc");
-				//トーストを表示。
 				Toast.makeText(MenuListActivity.this, desc, Toast.LENGTH_LONG).show();
 				break;
-			//［ご注文］メニューが選択された時の処理。
 			case R.id.menuListContextOrder:
-				//注文処理。
 				order(menu);
 				break;
 		}
-		//親クラスの同名メソッドを呼び出し、その戻り値を返却。
 		return super.onContextItemSelected(item);
 	}
 
-	/**
-	 * リストビューに表示させる定食リストデータを生成するメソッド。
-	 *
-	 * @return 生成された定食リストデータ。
-	 */
 	private List<Map<String, Object>> createTeishokuList() {
-		//定食メニューリスト用のListオブジェクトを用意。
 		List<Map<String, Object>> menuList = new ArrayList<>();
 
-		//「から揚げ定食」のデータを格納するMapオブジェクトの用意とmenuListへのデータ登録。
 		Map<String, Object> menu = new HashMap<>();
 		menu.put("name", "から揚げ定食");
 		menu.put("price", 800);
 		menu.put("desc", "若鳥のから揚げにサラダ、ご飯とお味噌汁が付きます。");
 		menuList.add(menu);
 
-		//「ハンバーグ定食」のデータを格納するMapオブジェクトの用意とmenuListへのデータ登録。
 		menu = new HashMap<>();
 		menu.put("name", "ハンバーグ定食");
 		menu.put("price", 850);
 		menu.put("desc", "手ごねハンバーグにサラダ、ご飯とお味噌汁が付きます。");
 		menuList.add(menu);
 
-		//以下データ登録の繰り返し。
 		menu = new HashMap<>();
 		menu.put("name", "生姜焼き定食");
 		menu.put("price", 850);
@@ -220,30 +167,21 @@ public class MenuListActivity extends AppCompatActivity {
 		return menuList;
 	}
 
-	/**
-	 * リストビューに表示させるカレーリストデータを生成するメソッド。
-	 *
-	 * @return 生成されたカレーリストデータ。
-	 */
 	private List<Map<String, Object>> createCurryList() {
-		//カレーメニューリスト用のListオブジェクトを用意。
 		List<Map<String, Object>> menuList = new ArrayList<>();
 
-		//「ビーフカレー」のデータを格納するMapオブジェクトの用意とmenuListへのデータ登録。
 		Map<String, Object> menu = new HashMap<>();
 		menu.put("name", "ビーフカレー");
 		menu.put("price", 520);
 		menu.put("desc", "特選スパイスをきかせた国産ビーフ100%のカレーです。");
 		menuList.add(menu);
 
-		//「ポークカレー」のデータを格納するMapオブジェクトの用意とmenuListへのデータ登録。
 		menu = new HashMap<>();
 		menu.put("name", "ポークカレー");
 		menu.put("price", 420);
 		menu.put("desc", "特選スパイスをきかせた国産ポーク100%のカレーです。");
 		menuList.add(menu);
 
-		//以下データ登録の繰り返し。
 		menu = new HashMap<>();
 		menu.put("name", "ハンバーグカレー");
 		menu.put("price", 620);
@@ -277,36 +215,23 @@ public class MenuListActivity extends AppCompatActivity {
 		return menuList;
 	}
 
-	/**
-	 * 注文処理を行うメソッド。
-	 *
-	 * @param menu 注文するメニューを表すMapオブジェクト。
-	 */
+
 	private void order(Map<String, Object> menu) {
-		//定食名と金額を取得。Mapの値部分がObject型なのでキャストが必要。
 		String menuName = (String) menu.get("name");
 		Integer menuPrice = (Integer) menu.get("price");
 
-		//インテントオブジェクトを生成。
 		Intent intent = new Intent(MenuListActivity.this, MenuThanksActivity.class);
-		//第2画面に送るデータを格納。
 		intent.putExtra("menuName", menuName);
-		//MenuThanksActivityでのデータ受け取りと合わせるために、金額にここで「円」を追加する。
 		intent.putExtra("menuPrice", menuPrice + "円");
-		//第2画面の起動。
 		startActivity(intent);
 	}
 
-	/**
-	 * リストがタップされたときの処理が記述されたメンバクラス。
-	 */
+
 	private class ListItemClickListener implements AdapterView.OnItemClickListener {
 
 		@Override
 		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-			//タップされた行のデータを取得。
 			Map<String, Object> item = (Map<String, Object>) parent.getItemAtPosition(position);
-			//注文処理。
 			order(item);
 		}
 	}
